bf3c6da993f26131bf690c57880f156a19b854bd
fauxy87 <145566772+fauxy87@users.noreply.github.com>
2026-09-23 13:10:31 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,10 @@
-const CACHE = 'sutton-cc-tournament-manager-v5';
-const FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './sw.js',
-  './icon.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => key !== CACHE ? caches.delete(key) : null)
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+const CACHE='sutton-tournament-v7-2';
+const CORE=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{
+  const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;
+ }).catch(()=>caches.match('./index.html'))));
 });
